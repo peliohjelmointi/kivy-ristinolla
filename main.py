919ff407313b8e185181d02af6d049b7ea82138d
Kalle S <KS@gmail.com>
2024-04-12 11:11:14 +0300

    add ellipse

--- main.py
+++ main.py
@@ -5,6 +5,19 @@ from kivy.graphics import Ellipse
 class MyWidget(Widget):
     def __init__(self):
         super().__init__()
+        # self.bind(pos=self.update_canvas)
+        # self.bind(size=self.update_canvas)       
+        self.update_canvas()        
+       
+    def update_canvas(self, *args):
+        self.canvas.clear() #tyhjää canvaksen
+        with self.canvas: #mitä canvakselle piirretään
+            #Ellipse(pos=self.pos, size=self.size)
+            self.e =Ellipse()            
+
+    def on_size(self,*args):#kutsutaan, kun ikkunan koko muuttuu
+        self.e.pos = self.pos
+        self.e.size = self.size
 
 class MyApp(App): #pääohjelma, (jos)kv-file:myapp
     def build(self): # tätä kutsutaan, MyApp kutsutaan
